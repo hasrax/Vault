a9a7fb97a2518ec1bcf3564541dbac820b3dfdf9
hasrax <hasragunawardena093@gmail.com>
2026-03-26 11:06:39 +0530

    Initial Commit

---
+++ Budgeting-App/Budgeting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgeting_AppApp.swift
+//  Budgeting-App
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-03-26.
+//
+
+import SwiftUI
+
+@main
+struct Budgeting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgeting-App
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
