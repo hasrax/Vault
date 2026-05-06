0b136f71e87a1f6b2284458cc84810aefb4ef535
hasrax <hasragunawardena093@gmail.com>
2026-05-06 16:49:49 +0530

    added unit tests

---
+++ Budgeting-AppTests/Budgeting_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Budgeting_AppTests.swift
+//  Budgeting-AppTests
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-05-06.
+//
+
+import Testing
+
+struct Budgeting_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
